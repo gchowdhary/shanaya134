9adb7d6185b2d857898608018f07527149dc9915
gchowdhary <76491707+gchowdhary@users.noreply.github.com>
2022-09-20 20:26:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+i=0
+
+function preload() {
+    img = loadImage("baby.jpg")
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    // document.getElementById("status").innerHTML = "Baby Not Found";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+  }
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+          if(status != "")
+        {
+          for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+              fill("#FF0000");
+             percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x - 15, objects[i].y - 15);
+            noFill(); //unset the colour
+            stroke("#FF0000"); //border colour
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+          }
+        }
+  }
+  
